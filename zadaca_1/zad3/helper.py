b6a1f9d2e393b0a0515bfccb9e1992b6d052d59a
rerceg <roko.erceg610@gmail.com>
2023-03-16 21:39:33 +0100

    zad3 - done

--- zadaca_1/zad3/helper.py
+++ zadaca_1/zad3/helper.py
@@ -1,34 +1,19 @@
 import numpy as np
 
-def printHoursArms():
+def printClockArms(radius, nOfPoints, name):
 	center = np.array([256, 127])
-	r = 50
 
-	angles = np.array([(2*np.pi*(i/720) - np.pi/2) for i in range(720)])
+	angles = np.array([(2*np.pi*(i/nOfPoints) - np.pi/2) for i in range(nOfPoints)])
 
-	discretizedCircle = np.zeros((720,2))
+	discretizedCircle = np.zeros((nOfPoints,2))
 
 	for i, angle in enumerate(angles):
-		x = int(round(center[0] + r*np.cos(angle)))
-		y = int(round(center[1] + r*np.sin(angle)))
-		print("let hoursPositionsX[" + str(i) + "] = "+ str(x) + ";")
-		print("let hoursPositionsY[" + str(i) + "] = "+ str(y) + ";")
-
-def printMinutesArms():
-	center = np.array([256, 127])
-	r = 75
-
-	angles = np.array([(2*np.pi*(i/60) - np.pi/2) for i in range(60)])
-
-	discretizedCircle = np.zeros((60,2))
-
-	for i, angle in enumerate(angles):
-		x = int(round(center[0] + r*np.cos(angle)))
-		y = int(round(center[1] + r*np.sin(angle)))
-		print("let minutesPositionsX[" + str(i) + "] = "+ str(x) + ";")
-		print("let minutesPositionsY[" + str(i) + "] = "+ str(y) + ";")
+		x = int(round(center[0] + radius*np.cos(angle)))
+		y = int(round(center[1] + radius*np.sin(angle)))
+		print("let " + name + "PositionsX[" + str(i) + "] = "+ str(x) + ";")
+		print("let " + name + "PositionsY[" + str(i) + "] = "+ str(y) + ";")
 
 
 if __name__ == '__main__':
-	#printHoursArms()
-	printMinutesArms()
+	printClockArms(50, 720, "hours")
+	printClockArms(75, 60, "minutes")
